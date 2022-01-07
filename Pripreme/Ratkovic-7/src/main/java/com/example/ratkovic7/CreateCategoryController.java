cc2107262d529e6b5f2dcfb45fb2700528bd9424
lukaratkovic <luka.ratkovic09@gmail.com>
2022-01-07 05:21:53 +0100

    Priprema8 Category creation validity check
    All entered Category data is now checked before being saved to the file, and a dialog is displayed depending on the result

--- Pripreme/Ratkovic-7/src/main/java/com/example/ratkovic7/CreateCategoryController.java
+++ Pripreme/Ratkovic-7/src/main/java/com/example/ratkovic7/CreateCategoryController.java
@@ -2,6 +2,7 @@ package com.example.ratkovic7;
 
 import hr.java.production.model.Category;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 
 import java.io.FileWriter;
@@ -21,24 +22,53 @@ public class CreateCategoryController {
     TextField categoryDescriptionTextField;
 
     @FXML
-    public void initialize(){
+    public void initialize() {
         categories = loadCategories();
     }
 
     @FXML
-    protected void onSaveButtonClick(){
-        //TODO: Checks for validity of fields before saving
-        String enteredName = categoryNameTextField.getText();
-        String enteredDescription = categoryDescriptionTextField.getText();
-        Long id = categories.get(categories.size()-1).getId() + 1;
+    protected void onSaveButtonClick() {
+        StringBuilder recordValue = new StringBuilder();
+        StringBuilder errorMessage = new StringBuilder();
 
-        try(FileWriter categoryFileWriter = new FileWriter(CATEGORY_FILE, true)){
-            categoryFileWriter.write(id+"\n"+enteredName+"\n"+enteredDescription+"\n");
-        }catch (IOException e){
-            e.printStackTrace();
+        Long id;
+        if (categories.size() < 1) id = 1L;
+        else id = categories.get(categories.size() - 1).getId() + 1;
+        recordValue.append(id.toString());
+        recordValue.append("\n");
+
+        if (categoryNameTextField.getText().isEmpty()) {
+            errorMessage.append("Category name should not be empty!\n");
+        } else {
+            recordValue.append(categoryNameTextField.getText());
+            recordValue.append("\n");
         }
 
-        categoryNameTextField.setText("");
-        categoryDescriptionTextField.setText("");
+        if (categoryDescriptionTextField.getText().isEmpty()) {
+            errorMessage.append("Category description should not be empty!\n");
+        } else {
+            recordValue.append(categoryDescriptionTextField.getText());
+            recordValue.append("\n");
+        }
+        if (errorMessage.isEmpty()) {
+            try (FileWriter categoryFileWriter = new FileWriter(CATEGORY_FILE, true)) {
+                categoryFileWriter.write(recordValue.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Save action successful!");
+            alert.setHeaderText("Category successfully saved");
+            alert.setContentText(new StringBuilder().append("Category ").append(categoryNameTextField.getText()).append(" successfully saved").toString());
+            alert.showAndWait();
+            categoryNameTextField.setText("");
+            categoryDescriptionTextField.setText("");
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Save action failed!");
+            alert.setHeaderText("Category could not be saved");
+            alert.setContentText(errorMessage.toString());
+            alert.showAndWait();
+        }
     }
 }
