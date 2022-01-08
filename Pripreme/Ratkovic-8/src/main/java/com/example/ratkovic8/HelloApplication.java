94e1b815b792f844af90bb173c26abf371f0965f
lukaratkovic <luka.ratkovic09@gmail.com>
2022-01-08 11:13:40 +0100

    Re-added logger

--- Pripreme/Ratkovic-8/src/main/java/com/example/ratkovic8/HelloApplication.java
+++ Pripreme/Ratkovic-8/src/main/java/com/example/ratkovic8/HelloApplication.java
@@ -4,14 +4,19 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     private static Stage mainStage;
 
+    public static final Logger logger = LoggerFactory.getLogger(HelloApplication.class);
+
     @Override
     public void start(Stage stage) throws IOException {
+        logger.info("Application started");
         mainStage = stage;
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("mainView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
--- Pripreme/Ratkovic-8/src/main/java/module-info.java
+++ Pripreme/Ratkovic-8/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.ratkovic7 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.slf4j;
 
 
     opens com.example.ratkovic8 to javafx.fxml;
