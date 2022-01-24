5b7cfbf2d8f62b2d6c4f969c8e7246b2e6ae7099
lukaratkovic <luka.ratkovic09@gmail.com>
2022-01-24 18:31:16 +0100

    vj9-2

--- Pripreme/Ratkovic-9/src/main/java/com/example/ratkovic8/OrderController.java
+++ Pripreme/Ratkovic-9/src/main/java/com/example/ratkovic8/OrderController.java
@@ -2,11 +2,16 @@ package com.example.ratkovic8;
 
 import hr.java.production.model.Order;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class OrderController {
     @FXML
@@ -37,7 +42,10 @@ public class OrderController {
         napomenaTableColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getNapomena()));
         datumTableColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getDatumKreiranja().toString()));
 
-        
+        List<String> slanjeOptions = new ArrayList<>();
+        slanjeOptions.add("HP");
+        slanjeOptions.add("Oversees Express");
+        slanjePutemComboBox.setItems(FXCollections.observableList(slanjeOptions));
     }
 
     @FXML
